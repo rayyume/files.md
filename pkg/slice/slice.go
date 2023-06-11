d6771c3c1bb603fd4d31123223a5e3be9aac9f48
Artem Zakirullin <artemzr@gmail.com>
2023-06-11 19:00:10 +0300

    add tests for chunk

--- pkg/slice/slice.go
+++ pkg/slice/slice.go
@@ -1,8 +1,14 @@
 package slice
 
-func Chunk[T any](items []T, chunkSize int) (chunks [][]T) {
-	for chunkSize < len(items) {
+func Chunk[T any](items []T, chunkSize int) [][]T {
+	if len(items) == 0 {
+		return [][]T{}
+	}
+
+	var chunks [][]T
+	for len(items) > chunkSize {
 		items, chunks = items[chunkSize:], append(chunks, items[0:chunkSize:chunkSize])
 	}
+
 	return append(chunks, items)
 }
--- pkg/slice/slice_test.go
+++ pkg/slice/slice_test.go
@@ -1,14 +1,44 @@
 package slice
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestSlice_chunk(t *testing.T) {
+func TestSmallerChunk(t *testing.T) {
+	// Test case 1: Chunk size is smaller than the length of the items slice
+	items := []int{1, 2, 3, 4, 5, 6}
+	chunkSize := 2
+	expectedChunks := [][]int{
+		{1, 2},
+		{3, 4},
+		{5, 6},
+	}
+	chunks := Chunk(items, chunkSize)
+	if !reflect.DeepEqual(chunks, expectedChunks) {
+		t.Errorf("Test case 1 failed. Expected chunks: %v, got: %v", expectedChunks, chunks)
+	}
+
+}
+
+func TestLargerChunk(t *testing.T) {
+	r := require.New(t)
+	r.Equal([][]int{{1, 2, 3, 4, 5, 6}}, Chunk([]int{1, 2, 3, 4, 5, 6}, 10))
+
+}
+
+func TestEqualChunk(t *testing.T) {
 	r := require.New(t)
 
-	s := []int{1, 2, 3, 4, 5}
-	r.Equal([][]int{{1, 2, 3}, {4, 5}}, Chunk(s, 3))
+	items := []int{1, 2, 3, 4, 5, 6}
+	chunkSize := len(items)
+	chunks := Chunk(items, chunkSize)
+	r.Equal([][]int{{1, 2, 3, 4, 5, 6}}, chunks)
+}
+
+func TestChunkEmpty(t *testing.T) {
+	r := require.New(t)
+	r.Equal([][]int{}, Chunk([]int{}, 2))
 }
